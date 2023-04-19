a86040f0720d27bd477eaf8746cde123086b8370
Angelo <angelo_tozzi@hotmail.com>
2023-04-19 09:00:06 -0300

    Validaciones Entidad

--- src/main/java/com/trimix/backend/models/entities/Persona.java
+++ src/main/java/com/trimix/backend/models/entities/Persona.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Setter;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
@@ -29,14 +31,24 @@ public class Persona {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "per_id")
     private Long perId;
+
+    @NotBlank
     @Column(name = "per_nombre")
     private String perNombre;
+
+    @NotBlank
     @Column(name = "per_apellido")
     private String perApellido;
+
+    @NotNull
     @Column(name = "per_numero_documento")
     private Long perNumeroDocumento;
+
+    @NotBlank
     @Column(name = "per_tipo_documento")
     private String perTipoDocumento;
+
+    @NotNull
     @Column(name = "per_fecha_nacimiento")
     private LocalDate perFechaNacimiento;
 
